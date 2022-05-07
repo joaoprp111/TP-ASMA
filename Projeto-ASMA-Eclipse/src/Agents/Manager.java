88c5d9465caabf96bea3066b4df08ebfd4aae8a6
joaoprp111 <joaoprp111@gmail.com>
2022-05-07 17:59:05 +0100

    Temos de começar a enviar os campos de visão

--- Projeto-ASMA-Eclipse/src/Agents/Manager.java
+++ Projeto-ASMA-Eclipse/src/Agents/Manager.java
@@ -7,14 +7,17 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class Manager extends Agent {
 	private static int N = 35;
 	private static int NUMTEAMS = 2;
-	//O tabuleiro vai apresentar os jogadores por A1, B1, etc
-	//Onde não estiver ninguém a ocupar posição fica string vazia
-	private Board board;
+	private boolean gameStarted = false;
+	private int currentRound = -1;
+	private int prevRound = -1;
+	private String currentPlayingTeam;
+	private Board board; //O tabuleiro vai apresentar os jogadores por A1, B1, etc; Onde não estiver ninguém a ocupar posição fica string "-"
 	private boolean knowsAllPlayers = false;
 	private HashMap<String,HashSet<AID>> teams = new HashMap<String,HashSet<AID>>();
 
@@ -65,8 +68,49 @@ public class Manager extends Agent {
 		public void action() {
 			board = new Board(N, teams);
 			System.out.println("Manager: Os jogadores ja estao prontos a jogar!");
+			currentRound = 0;
+			prevRound = currentRound;
+			gameStarted = true;
+			addBehaviour(new PrepareRoundBehaviour(myAgent, 1000));
 			//System.out.print(board.toString());
 		}
+	}
+	
+	private class PrepareRoundBehaviour extends TickerBehaviour{
+		public PrepareRoundBehaviour(Agent a, long period) {
+			super(a, period);
+		}
+		
+		public void onTick() {
+			if((currentRound == 0) || (currentRound > prevRound)) {
+				prevRound = currentRound;
+				changePlayingTeam();
+				sendVisionFields();
+			}
+		}
+	}
+	
+	private void changePlayingTeam() {
+		Set<String> teamIds = teams.keySet();
+		if(currentRound == 0) {
+			String[] arr = teamIds.toArray(new String[teamIds.size()]);
+			Random r = new Random();
+			int randNum = r.nextInt(teamIds.size());
+			this.currentPlayingTeam = arr[randNum];
+		} else {
+			Iterator<String> it = teamIds.iterator();
+			while(it.hasNext()) {
+				String team = it.next();
+				if (!team.equals(this.currentPlayingTeam)) {
+					this.currentPlayingTeam = team;
+					break;
+				}
+			}
+		}
+	}
+	
+	private void sendVisionFields() {
+		System.out.println("Falta enviar os campos de cada jogador para os respetivos treinadores!");
 	}
 	
 }
